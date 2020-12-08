89d64d38150a079da17e8ac5ae59d590582853f6
borodulex <borodulex@gmail.com>
2020-12-08 13:45:12 +0300

    refactor: cards preload

--- scripts/script.js
+++ scripts/script.js
@@ -12,6 +12,44 @@ let jobInput = popupNode.querySelector(".popup__input_type_job");
 
 let formElement = popupNode.querySelector(".popup__form");
 
+// Предварительная загрузка карточек при загрузке
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const cardTemplate = document.querySelector('#card-template');
+const cardsNode = document.querySelector('.cards__grid');
+
+initialCards.forEach(function (item) {
+  const cardElement = cardTemplate.content.cloneNode(true); // ? Почему cardElement === null если вынести эту строку вне тела фунцкции
+  cardElement.querySelector('.card__title').textContent = item.name;
+  cardElement.querySelector('.card__image').src = item.link;
+  cardsNode.append(cardElement);
+});
+
 // Открытие попапа при нажатии на кнопку редактирования
 function showPopup() {
   popupNode.classList.add("popup_opened");
@@ -26,7 +64,7 @@ function closePopup() {
   popupNode.classList.remove("popup_opened");
 }
 
-//Функцилнал popup
+//Функционал popup
 function formSubmitHandler(evt) {
   //Отмена стандартной отправки формы
   evt.preventDefault();
@@ -40,3 +78,11 @@ function formSubmitHandler(evt) {
 editButton.addEventListener("click", showPopup);
 closeButton.addEventListener("click", closePopup);
 formElement.addEventListener("submit", formSubmitHandler);
+
+
+
+//ТЕСТОВЫЙ ПОЛИГОН
+
+
+
+
